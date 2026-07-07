28fc052b4bd15e6c2e4fa2bb2c66409a8634913f
karthiktcp <tcp.karthik@gmail.com>
2026-07-07 15:13:23 +0530

    site/ifa implementation

---
+++ app/(moto)/ifa/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
+import Premium from "./_components/Premium";    
+import About from "./_components/About";
+import Footer from "./_components/Footer";
+import AosInit from "./_components/AosInit";
+import Features from "./_components/Features";
+import Events from "./_components/Events";
+import Gallery from "./_components/Gallery";
+
+export const metadata = {
+  title: "Speedcar",
+  description: "Engineering Performance You Can Trust",
+};
+
+export default function Page() {
+  return (
+    <main className="relative min-h-screen bg-white overflow-x-hidden">
+      <AosInit />
+      <Header />
+      <Hero />
+      <About />
+      <Premium />
+      <Gallery />
+      <Features />
+      <Events />
+      <Footer />
+    </main>
+  );
+}
